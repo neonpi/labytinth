870637cc91f5810bb3274b7dfb0c21bda984a216
Luiz Fernando Nogueira <mnogueira.luizf@gmail.com>
2026-07-05 19:29:05 -0300

    feat: limit depth in DFS

--- dfs.py
+++ dfs.py
@@ -2,21 +2,27 @@ from entities import Maze, Node, is_exit
 from util import get_visited_dict, reconstruct_path
 
 
-def depth_first_search(maze: Maze) -> list[Node]:
+def depth_limited_search(maze: Maze) -> list[Node]:
+    depth_limit = len(maze.nodes)
     visited = get_visited_dict(maze)
-    stack = [maze.start()]
+    stack: list[tuple[Node, int]] = [(maze.start(), 0)]
     parent: dict[Node, Node] = {}
 
     while stack:
-        node = stack.pop()
+        node, depth = stack.pop()
+
         if visited[node]:
             continue
+
         visited[node] = True
+
         if is_exit(maze, node):
             break
-        for neighbor in node.edges:
-            if not visited[neighbor]:
-                parent[neighbor] = node
-                stack.append(neighbor)
+
+        if depth < depth_limit:
+            for neighbor in node.edges:
+                if not visited[neighbor]:
+                    parent[neighbor] = node
+                    stack.append((neighbor, depth + 1))
 
     return reconstruct_path(parent, maze.exit())
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sys
 from a_star import a_star_search
 from backtracking import backtracking_search
 from bfs import breadth_first_search
-from dfs import depth_first_search
+from dfs import depth_limited_search
 from greedy import greedy_search
 from ida_star import ida_star_search
 from maze_parser import read_maze
+from ordered_search import ordered_search
 from util import print_constructed_path
 
 input_file = ""
@@ -24,13 +25,15 @@ except FileNotFoundError:
 
 print("Backtracking:")
 print_constructed_path(backtracking_search(maze))
-print("BFS:")
+print("Busca em Largura:")
 print_constructed_path(breadth_first_search(maze))
-print("DFS:")
-print_constructed_path(depth_first_search(maze))
-print("Greedy:")
+print("Busca em Profundidade (Limitada):")
+print_constructed_path(depth_limited_search(maze))
+print("Busca Ordenada:")
+print_constructed_path(ordered_search(maze))
+print("Busca Gulosa:")
 print_constructed_path(greedy_search(maze))
-print("A*:")
+print("Busca A*:")
 print_constructed_path(a_star_search(maze))
-print("IDA*:")
+print("Busca IDA*:")
 print_constructed_path(ida_star_search(maze))
