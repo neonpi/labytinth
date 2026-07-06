c63cdfa0dadd2f98e66ab30c3de825c5d00fd665
Luiz Fernando Nogueira <mnogueira.luizf@gmail.com>
2026-07-06 11:11:25 -0300

    perf: use a heap instead of plain list

--- a_star.py
+++ a_star.py
@@ -1,3 +1,6 @@
+import heapq
+from itertools import count
+
 from entities import Maze, Node, is_exit
 from stats import SearchStats
 from util import distance, reconstruct_path
@@ -6,26 +9,28 @@ from util import distance, reconstruct_path
 def a_star_search(maze: Maze) -> tuple[list[Node], SearchStats]:
     """Performs the A* search of start to exit of the given maze returning the path constructed
     during the process"""
-    open: list[Node] = []
-    closed: list[Node] = []
+    tie_breaker = count()  # avoids comparing Node objects on f-value ties in the heap
+    open_heap: list[tuple[int, int, Node]] = []
+    closed: set[Node] = set()
     parent: dict[Node, Node] = {}
     g_evaluation: dict[Node, int] = {}
-    h_evaluation: dict[Node, int] = {}
-    f_evaluation: dict[Node, int] = {}
     stats = SearchStats()
 
-    open.append(maze.start())
     g_evaluation[maze.start()] = 0
-    h_evaluation[maze.start()] = distance(maze.start(), maze.exit())
-    f_evaluation[maze.start()] = g_evaluation[maze.start()] + h_evaluation[maze.start()]
+    heapq.heappush(open_heap, (distance(maze.start(), maze.exit()), next(tie_breaker), maze.start()))
 
-    while open:
-        node = min(open, key=lambda n: f_evaluation[n])
-        open.remove(node)
+    while open_heap:
+        _, _, node = heapq.heappop(open_heap)
+
+        if node in closed:
+            continue  # stale entry left over from a worse path, already finalized
+
         stats.nodes_expanded += 1
 
         if is_exit(maze, node):
             break
+
+        closed.add(node)
 
         for neighbor in node.edges:
             stats.nodes_visited += 1
@@ -34,16 +39,12 @@ def a_star_search(maze: Maze) -> tuple[list[Node], SearchStats]:
 
             temp_g = g_evaluation[node] + 1  # we're not using weighted edges
 
-            if neighbor not in open:
-                open.append(neighbor)
-            elif temp_g >= g_evaluation[neighbor]:
+            if neighbor in g_evaluation and temp_g >= g_evaluation[neighbor]:
                 continue  # worse path than the one to be explored later
 
             parent[neighbor] = node
             g_evaluation[neighbor] = temp_g
-            h_evaluation[neighbor] = distance(neighbor, maze.exit())
-            f_evaluation[neighbor] = g_evaluation[neighbor] + h_evaluation[neighbor]
-
-        closed.append(node)
+            f = temp_g + distance(neighbor, maze.exit())
+            heapq.heappush(open_heap, (f, next(tie_breaker), neighbor))
 
     return reconstruct_path(parent, maze.exit()), stats
